36a673adaca92c7be87330ef429028867739a4dc
Dioka <diokajr@gmail.com>
2023-10-25 17:07:40 +0100

    IdeaBox v1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {useState, useEffect,} from 'react';
 import './App.css';
 import Ideas from './Ideas';
 import Form from './Form';
@@ -20,6 +20,8 @@ function App(){
     const filteredIdeas = ideas.filter(idea => idea.id !== id);
     setIdeas(filteredIdeas)
   }
+
+  
 
   return (
     <main className="App">
